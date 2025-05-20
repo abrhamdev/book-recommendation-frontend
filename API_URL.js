19209c03920c95c4371040a55175ffb5cad700e3
Abrham <169100762+abrhamdev@users.noreply.github.com>
2025-05-20 09:46:57 +0300

    Update API_URL.js

--- API_URL.js
+++ API_URL.js
@@ -1,5 +1,5 @@
-//export const API_URL='https://api-gateway-c2xx.onrender.com'
-export const API_URL='http://localhost:5001'
+export const API_URL='https://api-gateway-c2xx.onrender.com'
+//export const API_URL='http://localhost:5001'
 
 export const SOCKET_URL='https://community-and-review.onrender.com';
 
