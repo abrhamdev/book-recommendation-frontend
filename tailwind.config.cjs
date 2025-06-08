197beb7575ccd913beb19f8037ffc0c9421841f6
Abrham A. <abrhamabebe564@gmail.com>
2025-06-08 16:14:57 +0300

    -

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,js,ts,jsx,tsx,vue}', // adjust depending on your stack
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
